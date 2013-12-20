78b62cde0e7b613ac48b9de950065fc8bdc23b9f
Vit Brunner <vit.brunner@gmail.com>
2013-12-20 14:52:06 +0000

    Improve install.sh weird situation handling

--- install.sh
+++ install.sh
@@ -5,18 +5,33 @@ DOTRC_DIR="dotrc"
 
 # clone
 git clone https://github.com/tasuk/dotrc.git "$DOTRC_DIR" &&
+
 # change
 cd "$DOTRC_DIR" &&
 
 # update
 git submodule init && git submodule update &&
+
 # return
 cd .. &&
 
 # link
 for DOTFILE in `ls -dA "$DOTRC_DIR"/.??* | grep -v "\(.gitmodules\|.git$\)"`; do
+	# original existing file
+	ORIG=`echo "$DOTFILE" | sed "s:$DOTRC_DIR/::g"`
+	if [ -h "$ORIG" ]; then
+		# original is a symlink
+		continue
+	fi
+
+	if [ -e "$ORIG" ]; then
+		# original exists - move to .orig
+		mv "$ORIG"{,.orig}
+	fi
 	ln -s "$DOTFILE"
 done
+
 # clean
 unset DOTRC_DIR DOTFILE
+
 # exit
