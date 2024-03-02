430eb3aa95aaa88a29be43fe3d022cbd9156ba0a
Vit Brunner <vit.brunner@gmail.com>
2024-03-02 10:35:50 +0100

    Gnome 3 internet things

--- gnome3-internet.sh
+++ gnome3-internet.sh
@@ -1,39 +1,27 @@
 #!/bin/bash
 
-# tested on Gnome 3.36.8 and... it doesn't work!
+# tested on Gnome 43.9 on Debian 12 bookworm... still can't get things to work properly
 
-echo "This ain't working... go install the extensions manually"
-exit 1 # no dice, go install the extensions manually
+echo "This is semi-working... requires pressing enter very fast"
+#exit 1 # no dice, go install the extensions manually
 
 set -euo pipefail
 
-EXTENSION_URL="https://extensions.gnome.org/extension-data"
-EXTENSION_PATH="$HOME/.local/share/gnome-shell/extensions/"
+# https://extensions.gnome.org/extension/1287/unite/
+# https://extensions.gnome.org/extension/1485/workspace-matrix/
+# https://extensions.gnome.org/extension/3010/system-monitor-next/
+# https://extensions.gnome.org/extension/779/clipboard-indicator/
+# https://extensions.gnome.org/extension/307/dash-to-dock/
 
-# no-title-barjonaspoehler.de.v5.shell-extension.zip - superseded by unite
-# remove-dropdown-arrowsmpdeimos.com.v13.shell-extension.zip - superseded by unite
 EXTENSIONS="
-unitehardpixel.eu.v48.shell-extension.zip
-wsmatrixmartin.zurowietz.de.v27.shell-extension.zip
-system-monitor-nextparadoxxx.zero.gmail.com.v4.shell-extension.zip
-clipboard-indicatortudmotu.com.v37.shell-extension.zip
+unite@hardpixel.eu
+wsmatrix@martin.zurowietz.de
+system-monitor-next@paradoxxx.zero.gmail.com
+clipboard-indicator@tudmotu.com
+dash-to-dock@micxgx.gmail.com
 "
 
-cd /tmp/
 for EXTENSION in $EXTENSIONS; do
-	echo "Downloading $EXTENSION"
-	curl -O "$EXTENSION_URL/$EXTENSION"
-	#gnome-extensions install "$EXTENSION"
-	UUID=$(unzip -qc "$EXTENSION" metadata.json | jq -r '.uuid')
-	echo $UUID
-
-	if [[ ! -d $EXTENSION_PATH/$UUID ]]; then
-		echo "Unzipping extension $UUID"
-		mkdir "$EXTENSION_PATH/$UUID"
-		unzip -q "./$EXTENSION" -d "$EXTENSION_PATH/$UUID"
-	else
-		echo "Extension $UUID already exists"
-	fi
-	# gnome-extensions enable "$UUID" # this should work but doesn't?
-	gnome-shell-extension-tool -e "$UUID" # this works for some extensions but wsmatrix breaks
+	busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION}
 done
+
