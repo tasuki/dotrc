cf1d2cd7bfd0b07812a5c277e486cb68d526218f
Vit Brunner <vit.brunner@gmail.com>
2024-03-20 19:36:38 +0100

    Fix Haskell and Scala

--- install/prog-haskell.sh
+++ install/prog-haskell.sh
@@ -6,8 +6,12 @@ source "$SCRIPT_DIR/functions.sh"
 
 sudo apt install build-essential curl libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 zlib1g-dev  code
 
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+if (command -v ghcup > /dev/null 2>&1); then
+	echo "GHCup is already installed"
+else
+	curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
-maybe_append_zshrc ghcup '[ -f "/home/vita/.ghcup/env" ] && . "/home/vita/.ghcup/env" # ghcup-env'
+	maybe_append_zshrc ghcup '[ -f "/home/vita/.ghcup/env" ] && . "/home/vita/.ghcup/env" # ghcup-env'
+fi
 
 code --install-extension haskell.haskell
--- install/prog-scala.sh
+++ install/prog-scala.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 source "$SCRIPT_DIR/functions.sh"
 
+sudo apt install default-jdk
+
 install_binary amm "https://github.com/lihaoyi/ammonite/releases/download/2.5.5/2.13-2.5.5"
 
 code --install-extension scalameta.metals
