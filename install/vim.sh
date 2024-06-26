673ff51acdb3cdffa816a5ff62a8b5ec6117bcdf
Vit Brunner <vit.brunner@gmail.com>
2024-06-26 23:04:46 +0200

    Install plugins with nvim if available

--- install/vim.sh
+++ install/vim.sh
@@ -7,7 +7,11 @@ set -euo pipefail
 		"https://raw.githubusercontent.com/junegunn/vim-plug/0.13.0/plug.vim"
 
 # install the plugins
-vim +PlugInstall +PlugClean +qall
+if (command -v nvim > /dev/null 2>&1); then
+	nvim +PlugInstall +PlugClean +qall
+else
+	vim +PlugInstall +PlugClean +qall
+fi
 
 # hnng, don't want to fork this, but damn are the all caps tab names annoying
 sed --in-place 's/NERD_tree_/tree_/g' "$HOME/.vim/plugged/nerdtree/lib/nerdtree/creator.vim"
