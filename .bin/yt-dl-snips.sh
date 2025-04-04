6f3b557d5e8907a3caaa0d7a5a7bd2f3893752cf
Vit Brunner <vit.brunner@gmail.com>
2025-04-04 22:22:57 +0200

    That thing is not
    See https://wiki.tasuki.org/comp/shortcuts/yt-dlp

--- .bin/yt-dl-snips.sh
+++
@@ -1,2 +0,0 @@
-# Download songs from a playlist
-youtube-dl -x --audio-format mp3 -o "%(playlist_index)s-%(title)s.%(ext)s" "https://www.youtube.com/playlist?list=PLoJ4K2AUb-t6FcY0XLlneofU83s0b2moS"
